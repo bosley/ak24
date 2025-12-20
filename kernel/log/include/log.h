78ebfff34096ef78e2416cc151188552d6ac3a7a
bosley <josh.bosley@proton.me>
2025-12-20 17:09:32 -0500

    platform

--- kernel/log/include/log.h
+++ kernel/log/include/log.h
@@ -156,14 +156,14 @@ const char *ak_log_level_string(ak_log_level_t level);
  *
  * @par Example:
  * @code
- * pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+ * AK_MUTEX log_mutex = AK_MUTEX_INITIALIZER;
  *
  * void log_lock(bool lock, void *udata) {
- *   pthread_mutex_t *mtx = (pthread_mutex_t *)udata;
+ *   AK_MUTEX *mtx = (AK_MUTEX *)udata;
  *   if (lock) {
- *     pthread_mutex_lock(mtx);
+ *     AK_MUTEX_LOCK(mtx);
  *   } else {
- *     pthread_mutex_unlock(mtx);
+ *     AK_MUTEX_UNLOCK(mtx);
  *   }
  * }
  *
